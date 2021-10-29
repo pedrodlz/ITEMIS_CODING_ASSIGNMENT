cd6e114394120e63a28c7cc69fbc50e36ff91e11
pedrodlz <pedro.zurgena28@hotmail.com>
2021-10-29 13:11:25 +0200

    PROBLEM-1 getProductTaxes return the correct taxes for the selected item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,14 @@ import * as ProductDB from '../assets/products.json' ;
 export class AppComponent {
   title = 'problem1';
   db = JSON.parse(JSON.stringify(ProductDB))["default"];
-  BASIC_TAX = "10";
-  IMPORT_TAX = "5";
+  BASIC_TAX = 10;
+  IMPORT_TAX = 5;
   
   exemptProducts = ["books","food","medical"]
+
+  constructor(){
+    console.log(this.getProductTaxes("i_bottle_perfume_2"))
+  }
 
   getProduct(id:any){
     var product = this.db[id];
@@ -21,5 +25,16 @@ export class AppComponent {
 
   getProductTaxes(id:any){
     var product = this.getProduct(id);
+    var tax = 0;
+
+    if(!this.exemptProducts.includes(product.category))
+      tax += this.BASIC_TAX;
+    if(product.imported == "true")
+      tax += this.IMPORT_TAX;
+    
+    var productTax = ((tax*product.price)/100)
+    var roundedProductTax = (Math.ceil(productTax*20)/20).toFixed(2);
+
+    return roundedProductTax;
   }
 }
