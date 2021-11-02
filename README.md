052ea15a97db753265b6199eaf16368690d75f1f
pedrodlz <pedro.zurgena28@hotmail.com>
2021-11-02 14:16:46 +0100

    PROBLEM-1 Added code documentation and completed README with missing information

--- README.md
+++ README.md
@@ -1,10 +1,39 @@
 # Problem 1 - Sales Taxes
-Coding assignment for Itemis where is asked to develop an application that prints out a products receipt with the corresponding taxes for each product, following a list of rules.
+
+Coding assignment for Itemis where is asked to develop an application that prints out the receipt of a shopping list with the corresponding taxes for each product, following a list of rules.
+
+I have choose this exercise because it was the first one.
 
 ## Assumptions
-The first thing to assume is that this is a real-world software. Taking that in mind a database is needed in order to provide the corresponding products with their information. This database is written in a JSON file called **products.json**
 
-With the database ready the next step is to create the application
+The first thing to assume is that this is real-world software. Taking that in mind a database is needed to provide the corresponding products with their information. This database is written in a JSON file stored in **src/assets/products.json** With the database ready the next step is to create the application. 
+
+The application has been developed using Angular because it is easier to test and build than with pure Javascript and HTML, also there is possible to continue adding more features easily.
+
+This software does not have a "beautiful" stylesheet because it has not been asked in the exercise statement, but it would be easily added with Angular Material.
+
+## Problem solution
+
+All the followed steps have been described in the commits messages. 
+
+### Page window
+
+The resolution of the problem consists of a page with four sections. The first three sections show the three example inputs of the problem exercise. The fourth section is an extra tool to develop custom shopping lists easily with the existing products
 
 ## Run the application
-The application has been developed using Angular. In order to run the application open a powershell window in the problem folder and run the following commands:
+
+### Dependencies
+
+The application is built in Angular so to run it is necessary to have **NodeJS** installed
+
+### Steps
+
+To run the application it is necessary to open a PowerShell window in the root folder and run the following commands:
+
+> `npm install`
+
+This would install all the dependencies. After that run:
+
+> `ng serve`
+
+With this, the application will be served by default in  **localhost:4200**
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as ProductDB from '../assets/products.json' ;
 
-
+// Interface used for the products stored in the DB
 export interface Product{
   id:string,
   name:string,
@@ -10,6 +10,7 @@ export interface Product{
   imported:string
 }
 
+// Interface used for the products stores in the shopping list
 export interface ProductList{
   qty:number,
   id:string
@@ -22,41 +23,47 @@ export interface ProductList{
 })
 export class AppComponent {
   title = 'problem1';
-  db = JSON.parse(JSON.stringify(ProductDB))["default"];
-  productsList:Array<Product> = Object.values(this.db)
-  BASIC_TAX = 10;
-  IMPORT_TAX = 5;
-  exemptProducts = ["books","food","medical"]
 
-  selectedItem:any = "";
+  db = JSON.parse(JSON.stringify(ProductDB))["default"];  // DB export
+  productsList:Array<Product> = Object.values(this.db)    // Creattion of the selection list,
+                                                          // used for custom shopping list
+  BASIC_TAX = 10; // CONST base tax for all products except exempted ones
+  IMPORT_TAX = 5; // CONST extra tax for all impoted products
+  exemptProducts = ["books","food","medical"] // List of categories exempted from base tax
 
+  // Example input 1
   input1 = [
     {qty:1,id:"book"},
     {qty:1,id:"music_cd"},
     {qty:1,id:"chocolate_bar"}]
 
+  // Example input 2
   input2 = [
     {qty:1,id:"i_box_chocolates"},
     {qty:1,id:"i_bottle_perfume"}]
 
+  // Example input 3
   input3 = [
     {qty:1,id:"i_bottle_perfume_2"},
     {qty:1,id:"bottle_perfume"},
     {qty:1,id:"packet_headache_pill"},
     {qty:1,id:"i_box_imported_chocolates"}]
 
-  customInput:Array<ProductList> = [];
+  customInput:Array<ProductList> = [];  // Custom shopping list with the 
+                                        // products selected and quantities
 
   constructor(){
     //this.printReceipt(this.input2)
   }
 
+  // Given an id returns the product object
   getProduct(id:any){
     var product = this.db[id];
     if(product == undefined) return {price:0}
     return product;
   }
 
+  // Given the id of a product return the tax associated with that product
   getProductTaxes(id:any){
     var product = this.getProduct(id);
     var tax = 0;
@@ -66,17 +73,20 @@ export class AppComponent {
     if(product.imported == "true")
       tax += this.IMPORT_TAX;
     
-    var productTax = ((tax*product.price)/100)
-    var roundedProductTax = (Math.ceil(productTax*20)/20).toFixed(2);
+    var productTax = ((tax*product.price)/100)  //Base tax of product
+    var roundedProductTax = (Math.ceil(productTax*20)/20).toFixed(2); // Rounded tax up to 0.05
 
     return +roundedProductTax;
   }
 
+  // Given a shopping list object and a container, print the 
+  // resulting output in that given container
   printReceipt(selectedProducts:any, resultIDContainer:string){
     var salesTaxes = 0;
     var total = 0;
     var result = ""
 
+    // For each product calculate the taxes and add them to the total
     selectedProducts.forEach((plainProduct:any)=>{
       var product = this.getProduct(plainProduct.id);
       var taxes = this.getProductTaxes(plainProduct.id);
@@ -85,12 +95,12 @@ export class AppComponent {
       var productWithTaxes = (plainProduct.qty*(+product.price + taxes))
       total += +productWithTaxes;
 
-      console.log(plainProduct.qty + " " + product.name + ": " + productWithTaxes.toFixed(2))
+      console.debug(plainProduct.qty + " " + product.name + ": " + productWithTaxes.toFixed(2))
       result += plainProduct.qty + " " + product.name + ": " + productWithTaxes.toFixed(2) + "<br />"
     })
 
-    console.log("Sales Taxes: " + salesTaxes.toFixed(2));
-    console.log("Total: " + total.toFixed(2));
+    console.debug("Sales Taxes: " + salesTaxes.toFixed(2));
+    console.debug("Total: " + total.toFixed(2));
 
     result += "Sales Taxes: " + salesTaxes.toFixed(2) + "<br />";
     result += "Total: " + total.toFixed(2);
@@ -101,16 +111,19 @@ export class AppComponent {
     }
   }
 
+  // Add product to custom list
   addProduct(){
     this.customInput.push({qty:1,id:""})
   }
 
+  // Update quantity of a product
   updateQty(value:string,id:any){
     this.customInput[id].qty = +value;
   }
 
+  // Change product selected
   updateSelection(value:string,id:any){
-    console.log(value + id)
+    console.debug(value + id)
     this.customInput[id].id = value;
   }
 }
